18fface6b1859ba526108730c93bfe9055112030
Brigitte Jellinek <brigitte.jellinek@fh-salzburg.ac.at>
2015-10-16 09:14:31 +0200

    cleanup

--- potter.rb
+++ potter.rb
@@ -1,4 +1,11 @@
 #
+# Brigitte Jellineks solution to the "Harry Potter" Kata
+#
+
+# price for one potter book
+PRICE     = 8                           
+
+#
 # discount factor for i-1 distinct books,
 # e.g. for 3 different books a,b,c 
 # you will get a discount of DISCOUNT[2] == 0.95
@@ -6,12 +13,70 @@
 DISCOUNTS = [1, 0.95, 0.90, 0.80, 0.75]
 
 def potter(list_of_books)
-  counts = list_of_books.group_by{ |v| v }.values.map{ |list_of_v| list_of_v.length }.sort
+  counts = list_of_books.group_by{ |v| v }.values.map{ |list_of_v| list_of_v.length }
+  
+  bookogram = Bookogram.new( counts )
 
-  results = potter2( Bookogram.new( counts ), 5 )
+  results = potter2( bookogram, 5 )
 
   return results[0]   # first is price, second is structure
 end
+
+# ------------- recursive function -------------
+#
+# books is a Bookogram
+# level limits the level of discounts applied, e.g. level=4 means discount for 5 books is not used
+# set debug flag to true to see trace of the recursion
+#
+# this function returns TWO values:
+#   - the price that was computed
+#   - and array that describes the structure, how books were split up to apply the discounts
+# only the price is used by the external function, the seond value is only here
+# to enable debugging / tracing 
+
+def potter2(books, level=5, debug=false)
+
+  # end of recursion
+  if level == 1 or books.length <= 1
+    price = books.length * PRICE
+    return [price, [books.counts]]
+  end
+
+  n = books.biggest_set_of( level )
+
+  # we have to compare different alternatibes
+  # will collet the prices + structures into these arrays:
+  prices    = Array.new(n, 0)
+  structure = Array.new(n, [])
+
+  # when removing no books, descend one level
+  prices[0], structure[0] = potter2( books, level - 1, debug )
+
+  # when removing 1 to n books, no need to descend by level
+  (1..n).each do |i|
+    books_left_over = books.remove_i_layers_of_n_tupels( i, level ) 
+    price_r, structure_r = potter2( books_left_over, level, debug )
+    # total price = price for the removed books at current disount, plus price computed through recursion
+    prices[i]    = level * i * PRICE * DISCOUNTS[level-1] + price_r
+    # total structure = i times the tupel of current level, plus structure computed through recursion
+    structure[i] = [ Array.new( level, i ) ]               + structure_r
+  end
+
+  if debug and n > 0 
+    puts 
+    puts "-- there are #{n+1} alternatives i found through recursion for the #{books.length} books on level #{level}:"
+    (0..n).each do | i |
+      puts "%5.2f for %s" % [ prices[i], structure[i] ]
+    end
+    puts "--"
+  end
+
+  best_price = prices.min()
+  i = prices.rindex( best_price )
+  return best_price, structure[i]
+end
+
+
 
 
 class Bookogram
@@ -34,12 +99,20 @@ class Bookogram
   end
 
   def biggest_set_of(n)
+    # the book-counts are sorted by size, e.g:
+    # 4,3,2,2,0
+    # 2,0,0,0,0
+    # so the biggest set of - say - three books
+    # is found by just looking at the third element in the array
+    # 4,3,_2_,2,0 
+    # 2,0,_0_,0,0 
+    
     return @counts[n-1]
   end
 
-  def remove_first( x, y )
+  def remove_i_layers_of_n_tupels( i, n )
     c = @counts.clone
-    (0..x-1).each { |i| c[i] -= y }
+    (0..n-1).each { |k| c[k] -= i }
     return Bookogram.new(c)
   end
 
@@ -55,36 +128,5 @@ class Bookogram
     @counts.find_all { |count| count > 0 }
   end
 end
-
-
-def potter2(books, level=5)
-  if level == 1 or books.length <= 1
-    price = books.length * 8
-    return [price, [books.counts]]
-  end
-
-  n = books.biggest_set_of( level )
-
-  prices    = Array.new(n, 0)
-  structure = Array.new(n, [])
-  (0..n).each do |i|
-    books_left_over = books.remove_first( level, i ) 
-    results = potter2( books_left_over, level-1 )
-    prices[i]    = (level*i*8*DISCOUNTS[level-1]) + results[0]
-    structure[i] = results[1]
-    if i > 0 
-      structure[i].unshift( Array.new( level, i ) )
-    end
-  end
-  puts "there are #{n} possibilities:"
-  (0..n).each do | i |
-    puts "%5.2f for %s" % [ prices[i], structure[i] ]
-  end
-  puts "."
-  best_price = prices.min()
-  i = prices.rindex( best_price )
-  return best_price, structure[i]
-end
-
 
 
--- potter_test.rb
+++ potter_test.rb
@@ -3,11 +3,20 @@ require './potter'
 
 class PotterTest < MiniTest::Test
 
-  def test_main_potter
-    assert_equal 8 * 2, potter([ 'Sorcerers Stone', 'Sorcerers Stone' ])
-    assert_equal 8 * 2 * 0.95 + 8 * 1, potter([ 'Sorcerers Stone', 'Sorcerers Stone', 'Darthly Hallows' ]) 
-    assert_equal 8 * 5 * 0.75 + 8 * 1, potter([ 'Stone', 'Chamber', 'Prisoner', 'Goblet', 'Order', 'Prisoner' ])
-    assert_equal 8 * 4 * 0.80 * 2,     potter([ 'Stone', 'Stone', 'Chamber', 'Chamber', 'Prisoner', 'Prisoner', 'Goblet', 'Order' ])
+  def setup
+    # short hand for the discounted deals: TWO = price of two different books with discount applied
+    @one   = PRICE
+    @two   = PRICE * 2 * DISCOUNTS[ 2-1 ] 
+    @three = PRICE * 3 * DISCOUNTS[ 3-1 ] 
+    @four  = PRICE * 4 * DISCOUNTS[ 4-1 ] 
+    @five  = PRICE * 5 * DISCOUNTS[ 5-1 ] 
+  end
+
+  def test_potter_public_interface
+    assert_equal 2 * @one,     potter([ 'Sorcerers Stone', 'Sorcerers Stone' ])
+    assert_equal @two +  @one, potter([ 'Sorcerers Stone', 'Sorcerers Stone', 'Darthly Hallows' ]) 
+    assert_equal @five + @one, potter([ 'Stone', 'Chamber', 'Prisoner', 'Goblet', 'Order', 'Prisoner' ])
+    assert_equal 2 * @four,    potter([ 'Stone', 'Stone', 'Chamber', 'Chamber', 'Prisoner', 'Prisoner', 'Goblet', 'Order' ])
   end
 
   def test_bookogram
@@ -22,66 +31,75 @@ class PotterTest < MiniTest::Test
     assert_equal 0, Bookogram.new([4    ]).biggest_set_of(2)
   end
 
+
+  # ----------- tests for the internale "potter" function from here ---------------
+
   def test_zero
-    assert_equal [0, [[0,0,0,0,0]]], potter2( Bookogram.new([0,0,0,0,0]) )
+    assert_equal [0, [[0,0,0,0,0]]], potter2( Bookogram.new([0,0,0,0,0]))
+  end
+
+  def test_no_discount_any_level
+    (1..5).each do |level|
+      assert_equal [ 6 * @one, [[6,0,0,0,0]] ], potter2( Bookogram.new([6]), level)
+      assert_equal [ 5 * @one, [[5,0,0,0,0]] ], potter2( Bookogram.new([5]), level)
+      assert_equal [ 4 * @one, [[4,0,0,0,0]] ], potter2( Bookogram.new([4]), level)
+      assert_equal [ 3 * @one, [[3,0,0,0,0]] ], potter2( Bookogram.new([3]), level)
+    end
   end
 
   def test_level_1
-    assert_equal [ 8 * 1, [[1,0,0,0,0]] ], potter2( Bookogram.new([1,0,0,0,0]), 1)
-    assert_equal [ 8 * 2, [[1,1,0,0,0]] ], potter2( Bookogram.new([1,1,0,0,0]), 1)
-    assert_equal [ 8 * 2, [[2,0,0,0,0]] ], potter2( Bookogram.new([2,0,0,0,0]), 1)
-    assert_equal [ 8 * 3, [[2,1,0,0,0]] ], potter2( Bookogram.new([2,1,0,0,0]), 1)
-  end
-
-  def test_level_2_no_discount
-    (3..6).each do |i| assert_equal [ 8 * i, [[i,0,0,0,0]] ], potter2( Bookogram.new([i]), 2)
-    end
-  end
-
-  
-  def test_no_discount_any_level
-    (1..5).each do |level|
-      assert_equal [ 8 * 6, [[6,0,0,0,0]] ], potter2( Bookogram.new([6]), level)
-      assert_equal [ 8 * 5, [[5,0,0,0,0]] ], potter2( Bookogram.new([5]), level)
-      assert_equal [ 8 * 4, [[4,0,0,0,0]] ], potter2( Bookogram.new([4]), level)
-      assert_equal [ 8 * 3, [[3,0,0,0,0]] ], potter2( Bookogram.new([3]), level)
-    end
+    assert_equal [     @one, [[1,0,0,0,0]] ], potter2( Bookogram.new([1,0,0,0,0]), 1)
+    assert_equal [ 2 * @one, [[1,1,0,0,0]] ], potter2( Bookogram.new([1,1,0,0,0]), 1)
+    assert_equal [ 2 * @one, [[2,0,0,0,0]] ], potter2( Bookogram.new([2,0,0,0,0]), 1)
+    assert_equal [ 3 * @one, [[2,1,0,0,0]] ], potter2( Bookogram.new([2,1,0,0,0]), 1)
   end
 
   def test_discount_2
     (2..5).each do |level|
-      assert_equal [ 8 * 2 * 0.95 + 8 * 5, [[1,1], [5,0,0,0,0]] ],  potter2( Bookogram.new([6,1      ]), level)
-      assert_equal [ 8 * 2 * 0.95 + 8 * 4, [[1,1], [4,0,0,0,0]] ],  potter2( Bookogram.new([5,1      ]), level)
-      assert_equal [ 8 * 2 * 0.95 + 8 * 3, [[1,1], [3,0,0,0,0]] ],  potter2( Bookogram.new([4,1      ]), level)
-      assert_equal [ 8 * 2 * 0.95 + 8 * 2, [[1,1], [2,0,0,0,0]] ],  potter2( Bookogram.new([3,1      ]), level)
-      assert_equal [ 8 * 2 * 0.95 + 8 * 1, [[1,1], [1,0,0,0,0]] ],  potter2( Bookogram.new([2,1      ]), level)
+      assert_equal [ @two + 5 * @one, [[1,1], [5,0,0,0,0]] ],  potter2( Bookogram.new([6,1      ]), level)
+      assert_equal [ @two + 4 * @one, [[1,1], [4,0,0,0,0]] ],  potter2( Bookogram.new([5,1      ]), level)
+      assert_equal [ @two + 3 * @one, [[1,1], [3,0,0,0,0]] ],  potter2( Bookogram.new([4,1      ]), level)
+      assert_equal [ @two + 2 * @one, [[1,1], [2,0,0,0,0]] ],  potter2( Bookogram.new([3,1      ]), level)
+      assert_equal [ @two + 1 * @one, [[1,1], [1,0,0,0,0]] ],  potter2( Bookogram.new([2,1      ]), level)
     end
-    assert_equal [ 8 * 2 * 0.95 + 8,  [ [1,1],[1,0,0,0,0] ] ],  potter2( Bookogram.new([1,1,1    ]), 2)
-    # assert_equal 8 * 2 * 0.95 * 2, potter( Bookogram.new([1,1,1,1  ]), 2)
+    assert_equal [     @two + @one, [ [1,1],[1,0,0,0,0] ] ],  potter2( Bookogram.new([1,1,1    ]), 2)
   end
 
   def test_discount_3
     (3..5).each do |level|
-      assert_equal [ 8 * 3 * 0.90 + 8 * 5,                    [ [1,1,1], [5,0,0,0,0]        ] ], potter2( Bookogram.new([6,1,1      ]), level)
-      assert_equal [ 8 * 3 * 0.90 + 8 * 2 * 3 * 0.95 + 8 * 2, [ [1,1,1], [3,3], [2,0,0,0,0] ] ], potter2( Bookogram.new([6,4,1      ]), level)
+      assert_equal [ @three            + 5 * @one, [ [1,1,1], [5,0,0,0,0]        ] ], potter2( Bookogram.new([6,1,1      ]), level)
+      assert_equal [ @three + 3 * @two + 2 * @one, [ [1,1,1], [3,3], [2,0,0,0,0] ] ], potter2( Bookogram.new([6,4,1      ]), level)
     end
   end
 
   def test_discount_4
     (4..5).each do |level|
-      assert_equal [ 8 * 4 * 0.80 + 8 * 5,                    [ [1,1,1,1], [5,0,0,0,0]          ] ], potter2( Bookogram.new([6,1,1,1      ]), level)
-      assert_equal [ 8 * 4 * 0.80 + 8 * 2 * 3 * 0.95 + 8 * 2, [ [1,1,1,1], [3,3],   [2,0,0,0,0] ] ], potter2( Bookogram.new([6,4,1,1      ]), level)
-      assert_equal [ 8 * 4 * 0.80 + 8 * 3 * 3 * 0.90 + 8 * 2, [ [1,1,1,1], [3,3,3], [2,0,0,0,0] ] ], potter2( Bookogram.new([6,4,4,1      ]), level)
+      assert_equal [ @four              + 5 * @one, [ [1,1,1,1], [5,0,0,0,0]          ] ], potter2( Bookogram.new([6,1,1,1      ]), level)
+      assert_equal [ @four + 3 * @three + 2 * @one, [ [1,1,1,1], [3,3,3], [2,0,0,0,0] ] ], potter2( Bookogram.new([6,4,4,1      ]), level)
+      assert_equal [ @four + 3 * @two   + 2 * @one, [ [1,1,1,1], [3,3],   [2,0,0,0,0] ] ], potter2( Bookogram.new([6,4,1,1      ]), level)
     end
   end
 
-  def test_discount_4
-    assert_equal [ 8 * 5 * 0.75 + 8 * 1,                    [ [1,1,1,1,1], [1,0,0,0,0]          ] ], potter2( Bookogram.new([2,1,1,1,1]), 5)
-    assert_equal [ 8 * 5 * 0.75 + 8 * 5,                    [ [1,1,1,1,1], [5,0,0,0,0]          ] ], potter2( Bookogram.new([6,1,1,1,1]), 5)
-    assert_equal [ 8 * 5 * 0.75 + 8 * 2 * 3 * 0.95 + 8 * 2, [ [1,1,1,1,1], [3,3],   [2,0,0,0,0] ] ], potter2( Bookogram.new([6,4,1,1,1]), 5)
-    assert_equal [ 8 * 5 * 0.75 + 8 * 3 * 3 * 0.90 + 8 * 2, [ [1,1,1,1,1], [3,3,3], [2,0,0,0,0] ] ], potter2( Bookogram.new([6,4,4,1,1]), 5)
+  def test_discount_5
+    assert_equal [ @five            + @one,     [ [1,1,1,1,1], [1,0,0,0,0]          ] ], potter2( Bookogram.new([2,1,1,1,1]), 5)
+    assert_equal [ @five            + 5 * @one, [ [1,1,1,1,1], [5,0,0,0,0]          ] ], potter2( Bookogram.new([6,1,1,1,1]), 5)
+    assert_equal [ @five + 3 * @two + 2 * @one, [ [1,1,1,1,1], [3,3],   [2,0,0,0,0] ] ], potter2( Bookogram.new([6,4,1,1,1]), 5)
   end
-  def test_discount_horcurx
-    assert_equal [                8 * 4 * 2 * 0.80        , [ [1,1,1,1], [1,1,1,1]              ] ], potter2( Bookogram.new([2,2,2,1,1]), 5)
+
+  def test_horcurx 
+    # switch on debugging in this case, it's really intresting!!!
+    assert_equal [                2 * @four               , [ [1,1,1,1], [1,1,1,1], [0,0,0,0,0] ] ], potter2( Bookogram.new([2,2,2,1,1]), 5, false)
   end
+
+  def test_other_horcruxes
+    # found a cheaper price through testing, originally had [ [1,1,1,1,1], [3,3,3], [2,0,0,0,0]       ]
+    assert_equal [ 2 * @four + 2 * @three + 2 * @one,       [ [1,1,1,1],[1,1,1,1],[2,2,2],[2,0,0,0,0]]], potter2( Bookogram.new([6,4,4,1,1]), 5)
+    assert_equal [ 2 * @four + 2 * @two   + 2 * @one,       [ [1,1,1,1],[1,1,1,1],[2,2],  [2,0,0,0,0]]], potter2( Bookogram.new([6,4,2,1,1]), 5)
+  end
+
+  def test_simple
+    assert_equal [ 2 * @two,        [ [1,1],[1,1],[0,0,0,0,0] ] ],  potter2( Bookogram.new([1,1,1,1    ]), 2)
+  end
+
+
 end
